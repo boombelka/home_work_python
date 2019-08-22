c1c81d48f2077c6419fdb1ce770078dd873c2833
boombelka <av_cons@inbox.ru>
2019-08-22 08:51:01 +0300

    lessom3. сделал домашнее задание easy Сделал домашнее задание normal

--- hw03_hard.py
+++ hw03_hard.py
@@ -8,8 +8,48 @@
 # Вывод: 1 17/42  (результат обязательно упростить и выделить целую часть)
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
+"""
+
+def fraction(operation):
+
+    list = []
+    finger1 = []
+    finger2 = []
+    list = operation.split(' ')
+    print(list)
+    finger1 = list[0].split('/')
+    print(finger1)
+    finger2 = list[2].split('/')
+    print(finger2)
+    if finger1[1] == '/' and finger2[1] == '/':
+        down_part = int(finger1[2]) * int(finger2[2])
+        if list[1] == '+':
+            up_part = int(finger1[0]) * down_part + int(finger2[0]) * down_part
+        else:
+            up_part = int(finger1[0]) * down_part + int(finger2[0]) * down_part
+    elif finger1[1] == '/' and len(finger) == 1:
+        down_part = int(finger1[2])
+        if list[1] == '+':
+            up_part = int(finger1[0]) * down_part + int(finger2[0]) * down_part
+        else:
+            up_part = int(finger1[0]) * down_part - int(finger2[0]) * down_part
+    elif len(finger1) == 1 and finger2[1] == '/':
+        down_part = int(finger2[2])
+        if list[1] == '+':
+            up_part = int(finger1[0]) * down_part + int(finger2[0]) * down_part
+        else:
+            up_part = int(finger1[0]) * down_part + int(finger2[0]) * down_part
+    else:
+        pass
+#    full = up_part // down_part
+#    up_part_end = up_part - down_part * full
+#    print(f'{full}  {up_part_end}/{down_part}')
 
 
+print('Задание 1')
+operation = input(f'Введите выражение:')
+fraction(operation)
+"""
 # Задание-2:
 # Дана ведомость расчета заработной платы (файл "data/workers").
 # Рассчитайте зарплату всех работников, зная что они получат полный оклад,
@@ -17,7 +57,40 @@
 # то их ЗП уменьшается пропорционально, а за заждый час переработки
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
+def work_file():
 
+    list = []
+    basa = {}
+    with open('E:\python\home_work_python\data\workers.txt', 'r', encoding='UTF-8') as file:
+        #for i in range(0,4):
+        list = file.readlines()
+        #for line in file:
+        for i in range(0, 3):
+            m = list[i]
+            list[i] = m.split("|")
+        print(list)
+        norma = (str(list[0][1]))
+        for i in range(1, 3):
+            u = 0
+            figer = False
+            while figer == False:
+                n = 0
+                if list[i][u].isdigit() == True:
+                    n = n + list[i][u]
+                else:
+                    hour = n - norma
+                    if hour > 0:
+                        zp = hour/norma
+                        base[list[i][0]] = str(zp)
+                        break
+        print(base)
+
+
+def process():
+    pass
+
+
+work_file()
 
 # Задание-3:
 # Дан файл ("data/fruits") со списком фруктов.
